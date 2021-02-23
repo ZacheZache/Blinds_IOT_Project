2b3ebba64673da7b2670d96881983dfded95ee04
Kim Göransson <kim.goransson@student.kyh.se>
2021-02-23 11:29:25 +0100

    Added graphing

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, Markup, render_template
 import dynamo_sensor_db
 
 app = Flask(__name__)
+
+devices = [
+    'Device 1'
+]
+
+blind_positions = [
+    100, 0, 30, 25, 100, 100, 50
+]
+
+times = [
+    1, 2, 3, 4, 5, 6, 7
+]
+
 
 @app.route('/')
 def index():
@@ -12,6 +25,14 @@ def index():
 def get_dashboard():
     devices = dynamo_sensor_db.get_all_devices()
     return render_template('dashboard.html', devices=devices)
+
+
+@app.route("/test_data")
+def test_data():
+    blind_values = blind_positions
+    time_values = times
+    device_values = devices
+    return render_template('test_data.html', max=100, positions=blind_values, times=time_values, devices=device_values, title='DEVICE DATA')
 
 
 if __name__ == '__main__':
