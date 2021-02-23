d236b38fdb5bb5ec81126b3e39a88a20b7f93c0e
Kim Göransson <kim.goransson@student.kyh.se>
2021-02-23 16:25:13 +0100

    Divided data into two graphs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Markup, render_template
 import dynamo_sensor_db
+import json
 
 app = Flask(__name__)
 
@@ -31,10 +32,37 @@ def get_dashboard():
 
 @app.route("/test_data")
 def test_data():
-    blind_values = blind_positions
-    time_values = times
-    device_values = devices
-    return render_template('test_data.html', max=100, positions=blind_values, times=time_values, devices=device_values, title='DEVICE DATA')
+    entries = dynamo_sensor_db.get_all_devices('enviroment_data')
+    device_data = dynamo_sensor_db.get_all_devices('device_data')
+    devicelist = sorted(device_data, key=lambda k: k['Id'])
+    newlist = sorted(entries, key=lambda k: k['Id'])
+    azimuth_values = []
+    date_values = []
+    blind_positions = []
+    device_dates = []
+
+    for pos in devicelist:
+        newpos = pos['blind_position']
+        blind_positions.append(newpos)
+
+    for date in devicelist:
+        time = date['date']
+        device_dates.append(time)
+
+    for date in newlist:
+        time = date['date']
+        date_values.append(time)
+
+    for az in newlist:
+        azimuth = az['azimuth']
+        azimuth_values.append(azimuth)
+
+    device_values = newlist[0]['device_id']
+    device_name = newlist[0]['device_name']
+
+    return render_template('test_data.html', max=360, blind_max=100, blindpositions=blind_positions,
+                           blind_dates=device_dates, positions=azimuth_values, data_description=device_name,
+                           times=date_values, devices=device_values, title='DEVICE DATA')
 
 
 if __name__ == '__main__':
