cc15ecb5e522a72d8d9bc92fdf307a918a2cd924
David Ka <david.karnel@student.kyh.se>
2021-03-05 15:31:01 +0100

    lets goooo champ

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ times = [
 ]
 
 random_decimal = np.random.rand()
+device_name = None
 
 @app.route('/updated_ajax', methods=['POST'])
 def updateddecimal():
@@ -28,6 +29,56 @@ def updateddecimal():
 def ajax():
     return render_template('ajax.html', x=random_decimal)
 
+
+@app.route("/updated_data", methods=['GET'])
+def updated_data():
+    entries = dynamo_sensor_db.get_all_devices('enviroment_data')
+    device_data = dynamo_sensor_db.get_all_devices('device_data')
+
+    device_list = sorted(device_data, key=lambda k: k['Id'])
+    new_list = sorted(entries, key=lambda k: k['Id'])
+    newlist = new_list[-10:]
+    devicelist = device_list[-10:]
+    azimuth_values = []
+    date_values = []
+    blind_positions = []
+    device_dates = []
+    sun_in_win = []
+
+    for pos in devicelist:
+        newpos = pos['blind_position']
+        blind_positions.append(newpos)
+
+    for date in devicelist:
+        time = date['date']
+        device_dates.append(time)
+
+    for date in newlist:
+        time = date['date']
+        date_values.append(time)
+        sunStatus = date['sunInWin']
+        if sunStatus:
+            sun_in_win.append(360)
+        else:
+            sun_in_win.append(0)
+
+
+    for az in newlist:
+        azimuth = az['azimuth']
+        azimuth_values.append(azimuth)
+
+    device_values = newlist[0]['device_id']
+    device_name = newlist[0]['device_name']
+
+    return jsonify(render_template('data_display.html',  max=360, blind_max=100, blindpositions=blind_positions,
+                           blind_dates=device_dates, positions=azimuth_values, data_description=device_name,
+                           times=date_values, devices=device_values, sun_status=sun_in_win, title='DEVICE DATA'))
+
+
+
+@app.route('/data')
+def data():
+    return render_template('data.html', blindpositions=[])
 
 
 @app.route('/')
@@ -43,7 +94,7 @@ def get_dashboard():
     return render_template('dashboard.html', devices=devices)
 
 
-@app.route("/test_data")
+@app.route("/test_data_david")
 def test_data():
     entries = dynamo_sensor_db.get_all_devices('enviroment_data')
     device_data = dynamo_sensor_db.get_all_devices('device_data')
@@ -83,7 +134,7 @@ def test_data():
     device_values = newlist[0]['device_id']
     device_name = newlist[0]['device_name']
 
-    return render_template('test_data.html', max=360, blind_max=100, blindpositions=blind_positions,
+    return render_template('test_data_david.html', max=360, blind_max=100, blindpositions=blind_positions,
                            blind_dates=device_dates, positions=azimuth_values, data_description=device_name,
                            times=date_values, devices=device_values, sun_status=sun_in_win, title='DEVICE DATA')
 
